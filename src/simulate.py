268b4a3c9bc268568880ad789b7b5acb546c29b3
rokamu623 <r.okamura.061@ms.saitama-u.ac.jp>
2023-10-13 10:40:08 +0900

    feat: progress in varing parameter
    Signed-off-by: rokamu623 <r.okamura.061@ms.saitama-u.ac.jp>

--- src/simulate.py
+++ src/simulate.py
@@ -8,17 +8,17 @@ def simulate(dag: RDG_DAG, **kwargs):
     # implement each method here
 
     # the following is a sample
-    # dag_scheduling = Dag(dag.wcets, dag.edges)
-    # scheduler = Scheduler(5)
-    # scheduled_nodes: list[Node] = scheduler.schedule_dag(dag_scheduling)
+    dag_scheduling = Dag(dag.wcets, dag.edges)
+    scheduler = Scheduler(5)
+    scheduled_nodes: list[Node] = scheduler.schedule_dag(dag_scheduling)
 
     # for section 6
-    if kwargs['method'] == 'existing':
-        return 200
-    if kwargs['method'] == 'proposed':
-        dag_scheduling = Dag(dag.wcets, dag.edges)
-        scheduler = Scheduler(kwargs['core_num'])
-        scheduled_nodes: list[Node] = scheduler.schedule_dag(dag_scheduling)
+    # if kwargs['method'] == 'existing':
+    #     return 200
+    # if kwargs['method'] == 'proposed':
+    #     dag_scheduling = Dag(dag.wcets, dag.edges)
+    #     scheduler = Scheduler(kwargs['core_num'])
+    #     scheduled_nodes: list[Node] = scheduler.schedule_dag(dag_scheduling)
 
     # return evaluated value
     # in sample, response time of DAG (maximum finish time of all nodes)
@@ -73,9 +73,15 @@ def prepare_method_taskset(dags: list[RDG_DAG]):
 
 def evaluate_varing_parameter(dags: list[RDG_DAG], arg_dict: dict={}, **kwargs):
     if len(kwargs) == 0:
-        return [simulate(dag, **arg_dict) for dag in dags]
+        loop_len = len(dags)
+        results_list: list = []
+        for idx, dag in enumerate(dags):
+            results_list.append(simulate(dag, **arg_dict))
+            print("\r" + f'{idx+1} / {loop_len} : {100*(idx+1)/loop_len:.0f}% - {arg_dict}', end="")
+        print()
+        return results_list
     else:
-        results: dict = {}
+        results_dict: dict = {}
         parameter_name, parameter_values = list(kwargs.items())[0]
 
         kwargs_updated = kwargs.copy()
@@ -85,5 +91,5 @@ def evaluate_varing_parameter(dags: list[RDG_DAG], arg_dict: dict={}, **kwargs):
             arg_dict_updated = arg_dict.copy()
             arg_dict_updated[parameter_name] = v
 
-            results[v] = evaluate_varing_parameter(dags, arg_dict_updated, **kwargs_updated)
-        return results
+            results_dict[v] = evaluate_varing_parameter(dags, arg_dict_updated, **kwargs_updated)
+        return results_dict
