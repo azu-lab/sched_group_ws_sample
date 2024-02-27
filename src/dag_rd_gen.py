cbe481cd9a5a1c3c9f28d774e55c91bf7b99bdfa
rokamu623 <r.okamura.061@ms.saitama-u.ac.jp>
2024-02-27 10:56:20 +0900

    refactor: add comment for RDG_DAG
    Signed-off-by: rokamu623 <r.okamura.061@ms.saitama-u.ac.jp>

--- src/dag_rd_gen.py
+++ src/dag_rd_gen.py
@@ -2,29 +2,41 @@ from __future__ import annotations
 
 import yaml
 from networkx import DiGraph, all_simple_paths
-from PIL import Image
+from PIL.Image import Image, open as imgopen
 from glob import glob
 
 class RDG_DAG:
-    def __init__(self, wcets: list[int], edges: list[set[int]], rdgen_obj: dict, filepath: str, img: Image.Image):
-        self._wcets = wcets
-        self._edges = edges
-        self._rdgen_obj = rdgen_obj
-        self._filepath = filepath
-        self._img = img
+    def __init__(self, wcets: list[int], edges: list[set[int]], rdgen_obj: dict, filepath: str, img: Image | None):
+        """
+        Recommand to use RDG_DAG.load_from_rd_gen(filepath)
+        """
+        self._wcets: list[int] = wcets
+        self._edges: list[set[int]] = edges
+        self._rdgen_obj: dict = rdgen_obj
+        self._filepath: str = filepath
+        self._img: Image | None = img
 
-        self._graph = DiGraph()
+        self._graph: DiGraph = DiGraph()
         self._graph.add_nodes_from(range(len(wcets)))
         self._graph.add_edges_from(edges)
 
     def pred(self, idx: int) -> list[int]:
+        """
+        predecessor nodes of node idx
+        """
         return [_ for _ in self.graph.predecessors(idx)]
 
     def succ(self, idx: int) -> list[int]:
+        """
+        successor nodes of node idx
+        """
         return [_ for _ in self.graph.successors(idx)]
 
     @property
     def paths(self) -> list[list[int]]:
+        """
+        all end-to-end path of DAG
+        """
         _paths: list[list[int]] = []
         for sr in self.src:
             for sn in self.snk:
@@ -34,18 +46,30 @@ class RDG_DAG:
 
     @property
     def src(self) -> list[int]:
+        """
+        sourse nodes (have no predecessors)
+        """
         return [i for i in range(len(self.wcets)) if len(self.pred(i)) == 0]
 
     @property
     def snk(self) -> list[int]:
+        """
+        sink nodes (have no successors)
+        """
         return [i for i in range(len(self.wcets)) if len(self.succ(i)) == 0]
 
     @property
     def wcets(self) -> list[int]:
+        """
+        Worst-Case Execution Times
+        """
         return self._wcets
 
     @property
     def edges(self) -> list[set[int]]:
+        """
+        edges (idx, idx)
+        """
         return self._edges
 
     @property
@@ -57,18 +81,32 @@ class RDG_DAG:
 
     @property
     def filepath(self) -> str:
+        """
+        filepath of RD-Gen yaml
+        """
         return self._filepath
 
     @property
-    def img(self) -> Image.Image:
+    def img(self) -> Image:
+        """
+        image of DAG
+        """
+        if self._img is None:
+            assert('DAG has no image.')
         return self._img
 
     @property
     def graph(self) -> DiGraph:
+        """
+        DiGraph by networkx
+        """
         return self._graph
 
     @classmethod
     def load_from_rd_gen(cls, filepath: str, graphpath: str | None=None) -> RDG_DAG:
+        """
+        load DAG data from RD-Gen yaml and return RDG_DAG object
+        """
         try:
             file = open(filepath, 'r')
             rdg_obj = yaml.safe_load(file)
@@ -76,10 +114,12 @@ class RDG_DAG:
             print(f'RD-Gen: {filepath} not exists')
             exit()
 
-        if graphpath:
-            img = Image.open(graphpath)
-        else:
-            img = Image.open(filepath.replace('.yaml', '.png'))
+        if graphpath is None:
+            graphpath = filepath.replace('.yaml', '.png')
+        try:
+            img = imgopen(graphpath)
+        except:
+            img = None
 
         # define node
         wcets = [n['execution_time'] for n in sorted(rdg_obj['nodes'], key=lambda x: x['id'])]
@@ -88,7 +128,10 @@ class RDG_DAG:
         return RDG_DAG(wcets, edges, rdg_obj, filepath, img)
 
     @classmethod
-    def load_from_rd_gen_dir(cls, dirpath: str, regex: str='*'):
+    def load_from_rd_gen_dir(cls, dirpath: str, regex: str='*') -> list[RDG_DAG]:
+        """
+        load DAG data from all of RD-Gen yaml in dirpath directory and return RDG_DAG object list
+        """
         dags: list[RDG_DAG] = []
         for filepath in glob(f'{dirpath}/{regex}.yaml'):
             dags.append(cls.load_from_rd_gen(filepath))
